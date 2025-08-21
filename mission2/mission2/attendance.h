d9b8273ac579aaeaaad5dc2e0e1a909b1bf17379
Kim-Sung-Min87 <clever1501@naver.com>
2025-08-21 13:09:13 +0900

    mission2 project add

---
+++ mission2/mission2/attendance.h
@@ -0,0 +1,27 @@
+#pragma once
+
+enum Weekday {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    WEEKDAY_COUNT
+};
+
+const int MAX_PLAYERS = 21;
+
+const int GOLD_POINT = 50;
+const int SILVER_POINT = 30;
+
+const int GOLD_GRADE = 1;
+const int SILVER_GRADE = 2;
+const int NORMAL_GRADE = 0;
+
+const int BONUS_POINT = 10;
+const int BONUS_TRAINING_COUNT = 10;
+const int BONUS_WEEKEND_COUNT = 10;
+
+const int MAX_INPUT_NUM = 500;
